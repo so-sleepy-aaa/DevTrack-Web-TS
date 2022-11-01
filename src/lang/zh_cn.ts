f4102b7fd0b5ff37b5629f8d0a9285fbc72f30aa
阿夢 <ikaroinory233@gmail.com>
2022-11-01 13:15:13 +0800

    feat: Add zh_cn

--- src/lang/zh_cn.ts
+++ src/lang/zh_cn.ts
@@ -1,3 +1,32 @@
-const zh_cn = {};
+const zh_cn = {
+    signIn: {
+        title: "登录",
+        signIn: "登录DevTrack",
+        form: {
+            username: "用户名",
+            password: "密码",
+            signIn: "登录",
+            forgetPassword: "忘记密码",
+            noAccount: "没有账号？",
+            signUp: "点此注册"
+        }
+    },
+    signUp: {
+        title: "注册",
+        signUp: "注册AuroraLab账号",
+        form: {
+            username: "用户名",
+            password: "密码",
+            confirmPassword: "确认密码",
+            email: "邮箱",
+            vCode: "验证码",
+            getVCode: "获取验证码",
+            availableInXSecond: "x秒后可获取",
+            signUp: "注册",
+            haveAccount: "已有账号？",
+            signIn: "点此登录"
+        }
+    }
+};
 
 export default zh_cn;
